1ef9864f70ea48fdb47b7a385c0b2280a625a590
Josh Ryan <joshua.ryan@casa.gov.au>
2020-11-03 16:06:43 +1100

    roles and renewal updates

--- roles.js
+++ roles.js
@@ -9,6 +9,24 @@ if (hashURL=="CEO"){
     if (hashURL=="acc-manager"){
     rolename="Accountable Manager";
 }
+        if (hashURL=="cont-air-man"){
+    rolename="Continuing Airwortiness Manager";
+}
+        if (hashURL=="head-of-des"){
+    rolename="Head of Design";
+}
+            if (hashURL=="head-of-opp"){
+    rolename="Head of Operations";
+}
+            if (hashURL=="res-man"){
+    rolename="Responsible Manager";
+}
+            if (hashURL=="saf-man"){
+    rolename="Safety Manager";
+}
+            if (hashURL=="trai-man"){
+    rolename="Training Manager";
+}
     $(".role-name").text(rolename);
    var hashlink=$(".au-btn--primary").attr("href");
     hashlink=hashlink+"#"+hashURL;
